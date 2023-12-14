920cf31623c2c9311335feaa230207fc6d516e3f
Barnabás Szabó <barnabas.szabo1@tesco.com>
2023-12-14 13:14:30 +0100

    outlook + webmail

--- outlook-webapp/src/index.js
+++ outlook-webapp/src/index.js
@@ -6,27 +6,27 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+// const createWindow = () => {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js'),
+//     },
+//   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+//   // and load the index.html of the app.
+//   // mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
-};
+//   // Open the DevTools.
+//   // mainWindow.webContents.openDevTools();
+// };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -37,13 +37,13 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+// app.on('activate', () => {
+//   // On OS X it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow();
+//   }
+// });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
@@ -55,5 +55,15 @@ app.whenReady().then(() => {
   win.setBrowserView(view);
   view.setBounds({ x: 0, y: 0, width: 800, height: 600 });
   view.setAutoResize({width: true, height: true});
+  view.webContents.loadURL('https://outlook.office.com/mail/');
+});
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({ width: 800, height: 600 });
+
+  const view = new BrowserView();
+  win.setBrowserView(view);
+  view.setBounds({ x: 0, y: 0, width: 800, height: 600 });
+  view.setAutoResize({width: true, height: true});
   view.webContents.loadURL('https://outlook.office365.com/calendar/view/workweek');
-})
+});
